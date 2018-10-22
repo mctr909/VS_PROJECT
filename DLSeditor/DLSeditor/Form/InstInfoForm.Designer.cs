359c28ca8f04be9854c656956da465bea4e26a9f
松嵜義博 <tr-909@hotmail.co.jp>
2018-10-23 00:21:52 +0900

    追加

---
+++ DLSeditor/DLSeditor/Form/InstInfoForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿using System.Windows.Forms;
+
+namespace DLSeditor {
+	partial class InstInfoForm : Form {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstInfoForm));
+			this.tabControl = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.pnlRegion = new System.Windows.Forms.Panel();
+			this.pictRange = new System.Windows.Forms.PictureBox();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.tsbAddRange = new System.Windows.Forms.ToolStripButton();
+			this.tslPos = new System.Windows.Forms.ToolStripLabel();
+			this.tabControl.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.pnlRegion.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictRange)).BeginInit();
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl
+			// 
+			this.tabControl.Controls.Add(this.tabPage1);
+			this.tabControl.Controls.Add(this.tabPage2);
+			this.tabControl.Location = new System.Drawing.Point(12, 13);
+			this.tabControl.Name = "tabControl";
+			this.tabControl.SelectedIndex = 0;
+			this.tabControl.Size = new System.Drawing.Size(1477, 852);
+			this.tabControl.TabIndex = 2;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Location = new System.Drawing.Point(8, 39);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(1461, 805);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "音色設定";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.pnlRegion);
+			this.tabPage2.Controls.Add(this.toolStrip1);
+			this.tabPage2.Location = new System.Drawing.Point(8, 39);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(1461, 805);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "レイヤー設定";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// pnlRegion
+			// 
+			this.pnlRegion.AutoScroll = true;
+			this.pnlRegion.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pnlRegion.Controls.Add(this.pictRange);
+			this.pnlRegion.Location = new System.Drawing.Point(7, 44);
+			this.pnlRegion.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+			this.pnlRegion.Name = "pnlRegion";
+			this.pnlRegion.Size = new System.Drawing.Size(1408, 550);
+			this.pnlRegion.TabIndex = 4;
+			// 
+			// pictRange
+			// 
+			this.pictRange.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictRange.BackgroundImage")));
+			this.pictRange.InitialImage = null;
+			this.pictRange.Location = new System.Drawing.Point(0, 0);
+			this.pictRange.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+			this.pictRange.Name = "pictRange";
+			this.pictRange.Size = new System.Drawing.Size(1664, 768);
+			this.pictRange.TabIndex = 0;
+			this.pictRange.TabStop = false;
+			this.pictRange.DoubleClick += new System.EventHandler(this.pictRange_DoubleClick);
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+			this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbAddRange,
+            this.tslPos});
+			this.toolStrip1.Location = new System.Drawing.Point(3, 3);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+			this.toolStrip1.Size = new System.Drawing.Size(211, 35);
+			this.toolStrip1.TabIndex = 3;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// tsbAddRange
+			// 
+			this.tsbAddRange.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.tsbAddRange.Image = global::DLSeditor.Properties.Resources.plus;
+			this.tsbAddRange.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+			this.tsbAddRange.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.tsbAddRange.Name = "tsbAddRange";
+			this.tsbAddRange.Size = new System.Drawing.Size(23, 32);
+			this.tsbAddRange.Text = "toolStripButton1";
+			this.tsbAddRange.Click += new System.EventHandler(this.tsbAddRange_Click);
+			// 
+			// tslPos
+			// 
+			this.tslPos.Name = "tslPos";
+			this.tslPos.Size = new System.Drawing.Size(175, 32);
+			this.tslPos.Text = "toolStripLabel1";
+			// 
+			// InstInfoForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1998, 1029);
+			this.Controls.Add(this.tabControl);
+			this.Name = "InstInfoForm";
+			this.Text = "InstInfoForm";
+			this.SizeChanged += new System.EventHandler(this.InstInfoForm_SizeChanged);
+			this.tabControl.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.pnlRegion.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictRange)).EndInit();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TabControl tabControl;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton tsbAddRange;
+		private System.Windows.Forms.ToolStripLabel tslPos;
+		private System.Windows.Forms.Panel pnlRegion;
+		private System.Windows.Forms.PictureBox pictRange;
+	}
+}
---
+++ DLSeditor/DLSeditor/Form/InstInfoForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DLSeditor {
+	partial class InstInfoForm : Form {
+		private DLS.DLS mDLS;
+		private ListBox mList;
+		private int mIndex;
+
+		public InstInfoForm(DLS.DLS dls, ListBox list, int index) {
+			InitializeComponent();
+			SetTabSize();
+			mDLS = dls;
+			mList = list;
+			mIndex = index;
+			DispInstInfo();
+		}
+
+		private void InstInfoForm_SizeChanged(object sender, EventArgs e) {
+			SetTabSize();
+		}
+
+		#region サイズ調整
+		private void SetTabSize() {
+			var offsetX = 28;
+			var offsetY = 70;
+			var width = Width - offsetX;
+			var height = Height - offsetY;
+
+			if (width < 100) {
+				return;
+			}
+
+			if (height < 100) {
+				return;
+			}
+
+			tabControl.Width = width;
+			tabControl.Height = height;
+			SetInstAttributeSize();
+			SetInstRegionSize();
+		}
+
+		private void SetInstAttributeSize() {
+			var offsetX = 16;
+			var offsetY = 36;
+			var width = tabControl.Width - offsetX;
+			var height = tabControl.Height - offsetY;
+		}
+
+		private void SetInstRegionSize() {
+			var offsetX = 16;
+			var offsetY = 60;
+			var width = tabControl.Width - offsetX;
+			var height = tabControl.Height - offsetY;
+
+			pnlRegion.Width = width;
+			pnlRegion.Height = height;
+		}
+		#endregion
+
+		#region 音色設定
+		private void DispInstInfo() {
+			if (0 == mList.Items.Count) {
+				return;
+			}
+
+			var inst = mDLS.Instruments.List[GetLocale(mList, mList.SelectedIndex)];
+
+			Text = inst.Info.Name;
+
+			if (null != inst.Articulations && null != inst.Articulations.ART) {
+				foreach (var conn in inst.Articulations.ART.List.Values) {
+				}
+			}
+
+			DispRegionInfo();
+		}
+		#endregion
+
+		#region レイヤー設定
+		private void pictRange_DoubleClick(object sender, EventArgs e) {
+			if (mList.SelectedIndex < 0) {
+				return;
+			}
+
+			var cp = pictRange.PointToClient(Cursor.Position);
+			cp.X = (int)(cp.X / 6 + 0.5);
+			cp.Y = (int)((pictRange.Height - cp.Y) / 3 + 0.5);
+
+			DLS.RGN rgn;
+			var inst = mDLS.Instruments.List[GetLocale(mList, mList.SelectedIndex)];
+			foreach (var region in inst.Regions.List.Values) {
+				var key = region.Header.Key;
+				var vel = region.Header.Velocity;
+				if (key.Low <= cp.X && cp.X <= key.High
+				&& vel.Low <= cp.Y && cp.Y <= vel.High) {
+					rgn = region;
+					break;
+				}
+			}
+
+			tslPos.Text = string.Format("X:{0} Y:{1}", cp.X, cp.Y);
+		}
+
+		private void tsbAddRange_Click(object sender, EventArgs e) {
+
+		}
+
+		private void DispRegionInfo() {
+			var inst = mDLS.Instruments.List[GetLocale(mList, mList.SelectedIndex)];
+
+			var bmp = new Bitmap(pictRange.Width, pictRange.Height);
+			var g = Graphics.FromImage(bmp);
+			var blueLine = new Pen(Color.FromArgb(255, 0, 0, 255), 2.0f);
+			var greenFill = new Pen(Color.FromArgb(64, 0, 255, 0), 1.0f).Brush;
+
+			foreach (var region in inst.Regions.List.Values) {
+				var key = region.Header.Key;
+				var vel = region.Header.Velocity;
+				g.DrawRectangle(
+					blueLine,
+					key.Low * 6,
+					vel.Low * 3,
+					(key.High - key.Low + 1) * 6,
+					(vel.High - vel.Low + 1) * 3
+				);
+				g.FillRectangle(
+					greenFill,
+					key.Low * 6,
+					vel.Low * 3,
+					(key.High - key.Low + 1) * 6,
+					(vel.High - vel.Low + 1) * 3
+				);
+			}
+
+			pictRange.Image = bmp;
+		}
+		#endregion
+
+		public static DLS.MidiLocale GetLocale(ListBox list, int index) {
+			var cols = list.Items[index].ToString().Split('\t');
+
+			var locale = new DLS.MidiLocale();
+			locale.BankFlags = (byte)("Drum" == cols[0] ? 0x80 : 0x00);
+			locale.ProgramNo = byte.Parse(cols[1]);
+			locale.BankMSB = byte.Parse(cols[2]);
+			locale.BankLSB = byte.Parse(cols[3]);
+
+			return locale;
+		}
+	}
+}
