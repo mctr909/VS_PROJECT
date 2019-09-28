96cb078cb528d2e2b70f5f3751b1967bc79432e8
松崎好浩 <y.matsuzaki@ris.co.jp>
2019-09-28 10:16:14 +0900

    削除

--- IIRSpectrumAnalyzer/SpectrumAnalyzer/dft.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/******************************************************************************/
-class dft {
-public:
-    static int init(double baseFreq, double sigma, int notes, int octDiv, int length, int sampleRate);
-    static void purge();
-    static void exec(short *pInput, double *pAmp, int samples);
-};
