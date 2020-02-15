53b02d92c9dbc30bb4375689cc7447d3cac675d2
松崎好浩 <y.matsuzaki@ris.co.jp>
2020-02-15 20:40:43 +0900

    更新

--- 色変更/色変更/COLOR.cs
+++ 色変更/色変更/COLOR.cs
@@ -5,16 +5,14 @@ using System.Runtime.InteropServices;
 
 public class Color
 {
-    public struct HSL
-    {
+    public struct HSL {
         public int H;
         public int S;
         public int L;
         public byte A;
     }
 
-    public struct Param
-    {
+    public struct Param {
         public bool ChengeH;
         public int H;
         public int HWidth;
@@ -24,10 +22,8 @@ public class Color
         public int LMax;
     }
 
-    public struct Config
-    {
-        public Config(Param before, Param after)
-        {
+    public struct Config {
+        public Config(Param before, Param after) {
             Before = before;
             After = after;
         }
@@ -35,52 +31,41 @@ public class Color
         public Param After;
     }
 
-    public static void RGBtoHSL(ref byte b, ref byte g, ref byte r, ref byte a, ref HSL hsl)
-    {
+    public static void RGBtoHSL(ref byte b, ref byte g, ref byte r, ref byte a, ref HSL hsl) {
         byte max = Math.Max(r, Math.Max(g, b));
         byte min = Math.Min(r, Math.Min(g, b));
         int cnt = (max + min) / 2;
 
-        if (r == g && g == b)
-        {
+        if (r == g && g == b) {
             hsl.H = 0;
         }
-        else if (r == max)
-        {
+        else if (r == max) {
             hsl.H = 60 * (g - b) / (max - min);
         }
-        else if (g == max)
-        {
+        else if (g == max) {
             hsl.H = 60 * (b - r) / (max - min) + 120;
         }
-        else if (b == max)
-        {
+        else if (b == max) {
             hsl.H = 60 * (r - g) / (max - min) + 240;
         }
 
-        if (hsl.H < -180)
-        {
+        if (hsl.H < -180) {
             hsl.H += 360;
         }
-        else if(hsl.H > 180)
-        {
+        else if (hsl.H > 180) {
             hsl.H -= 360;
         }
 
-        if (cnt == 255)
-        {
+        if (cnt == 255) {
             hsl.S = 100;
         }
-        else if (cnt == 0)
-        {
+        else if (cnt == 0) {
             hsl.S = 0;
         }
-        else if (cnt >= 128)
-        {
+        else if (cnt >= 128) {
             hsl.S = 100 * (max - cnt) / (255 - cnt);
         }
-        else
-        {
+        else {
             hsl.S = 100 * (cnt - min) / cnt;
         }
 
@@ -89,71 +74,65 @@ public class Color
         hsl.A = a;
     }
 
-    public static void HSLtoRGB(ref HSL hsl, ref byte[] pix, ref int offset)
-    {
+    public static void HSLtoRGB(ref HSL hsl, ref byte[] pix, ref int offset) {
         float max, min, width;
 
-        if (hsl.L < 50)
-        {
+        if (hsl.L < 50) {
             max = 2.55f * (hsl.L + hsl.L * hsl.S / 100.0f);
             min = 2.55f * (hsl.L - hsl.L * hsl.S / 100.0f);
             width = (max - min) / 60.0f;
         }
-        else
-        {
+        else {
             max = 2.55f * (hsl.L + (100 - hsl.L) * hsl.S / 100.0f);
             min = 2.55f * (hsl.L - (100 - hsl.L) * hsl.S / 100.0f);
             width = (max - min) / 60.0f;
         }
 
-        if (hsl.H < 0)
-        {
+        if (hsl.H < 0) {
             hsl.H += 360;
         }
 
-        switch (hsl.H / 60)
-        {
-            case 0:
-                pix[offset] = (byte)min;
-                pix[offset + 1] = (byte)(hsl.H * width + min);
-                pix[offset + 2] = (byte)max;
-                pix[offset + 3] = hsl.A;
-                return;
-            case 1:
-                pix[offset] = (byte)min;
-                pix[offset + 1] = (byte)max;
-                pix[offset + 2] = (byte)((120 - hsl.H) * width + min);
-                pix[offset + 3] = hsl.A;
-                return;
-            case 2:
-                pix[offset] = (byte)((hsl.H - 120) * width + min);
-                pix[offset + 1] = (byte)max;
-                pix[offset + 2] = (byte)min;
-                pix[offset + 3] = hsl.A;
-                return;
-            case 3:
-                pix[offset] = (byte)max;
-                pix[offset + 1] = (byte)((240 - hsl.H) * width + min);
-                pix[offset + 2] = (byte)min;
-                pix[offset + 3] = hsl.A;
-                return;
-            case 4:
-                pix[offset] = (byte)max;
-                pix[offset + 1] = (byte)min;
-                pix[offset + 2] = (byte)((hsl.H - 240) * width + min);
-                pix[offset + 3] = hsl.A;
-                return;
-            default:
-                pix[offset] = (byte)((360 - hsl.H) * width + min);
-                pix[offset + 1] = (byte)min;
-                pix[offset + 2] = (byte)max;
-                pix[offset + 3] = hsl.A;
-                return;
+        switch (hsl.H / 60) {
+        case 0:
+            pix[offset] = (byte)min;
+            pix[offset + 1] = (byte)(hsl.H * width + min);
+            pix[offset + 2] = (byte)max;
+            pix[offset + 3] = hsl.A;
+            return;
+        case 1:
+            pix[offset] = (byte)min;
+            pix[offset + 1] = (byte)max;
+            pix[offset + 2] = (byte)((120 - hsl.H) * width + min);
+            pix[offset + 3] = hsl.A;
+            return;
+        case 2:
+            pix[offset] = (byte)((hsl.H - 120) * width + min);
+            pix[offset + 1] = (byte)max;
+            pix[offset + 2] = (byte)min;
+            pix[offset + 3] = hsl.A;
+            return;
+        case 3:
+            pix[offset] = (byte)max;
+            pix[offset + 1] = (byte)((240 - hsl.H) * width + min);
+            pix[offset + 2] = (byte)min;
+            pix[offset + 3] = hsl.A;
+            return;
+        case 4:
+            pix[offset] = (byte)max;
+            pix[offset + 1] = (byte)min;
+            pix[offset + 2] = (byte)((hsl.H - 240) * width + min);
+            pix[offset + 3] = hsl.A;
+            return;
+        default:
+            pix[offset] = (byte)((360 - hsl.H) * width + min);
+            pix[offset + 1] = (byte)min;
+            pix[offset + 2] = (byte)max;
+            pix[offset + 3] = hsl.A;
+            return;
         }
     }
 
-    public static Bitmap ChangeColor(Bitmap bmp, Config config)
-    {
+    public static Bitmap ChangeColor(Bitmap bmp, Config config) {
         if (bmp == null) return bmp;
 
         HSL hsl = new HSL();
@@ -178,10 +157,8 @@ public class Color
         float sWidth = (config.After.SMax - config.After.SMin) / 100.0f;
         float lWidth = (config.After.LMax - config.After.LMin) / 100.0f;
 
-        for (int py = 0; py < bmp.Height; ++py)
-        {
-            for (int px = 0, s = bmpRetData.Stride * py; px < bmp.Width; ++px, s += 4)
-            {
+        for (int py = 0; py < bmp.Height; ++py) {
+            for (int px = 0, s = bmpRetData.Stride * py; px < bmp.Width; ++px, s += 4) {
                 RGBtoHSL(ref pix[s], ref pix[s + 1], ref pix[s + 2], ref pix[s + 3], ref hsl);
 
                 hDiff = config.Before.H - hsl.H;
@@ -190,10 +167,8 @@ public class Color
 
                 if (Math.Abs(hDiff) <= config.Before.HWidth
                 && hsl.S >= config.Before.SMin && hsl.S <= config.Before.SMax
-                && hsl.L >= config.Before.LMin && hsl.L <= config.Before.LMax)
-                {
-                    if (config.After.ChengeH)
-                    {
+                && hsl.L >= config.Before.LMin && hsl.L <= config.Before.LMax) {
+                    if (config.After.ChengeH) {
                         hsl.H = config.After.H;
                     }
                     hsl.S = (int)(hsl.S * sWidth) + config.After.SMin;
